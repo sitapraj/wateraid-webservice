6f354c0883cb47abdcdef33c78e70500f3ae2f86
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-06-06 17:18:15 +0545

    Added new routes and views for the school_sanitation controllers

--- app/controllers/school_sanitation_controller.rb
+++ app/controllers/school_sanitation_controller.rb
@@ -2,10 +2,10 @@ class SchoolSanitationController < ApplicationController
   
   def index
     @districts = DistrictGirlSanitationCommunitySchool.all
-    respond_to do
+    respond_to do |format|
       format.html
       format.xml { render :xml => @districts }
-      format.json { render :json => @districts }
+      format.json { render :json =>  @districts}
     end
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 WateraidWebservice::Application.routes.draw do
   
-  namespace :api do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       resources :school_sanitation
     end
