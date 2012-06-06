c88b29356f329dc7cd2d477732d79a7c9e8fb0c5
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-06-06 15:49:10 +0545

    Added new Api school_sanitation for wateraid

--- app/controllers/api/v1/school_sanitation_controller.rb
+++ app/controllers/api/v1/school_sanitation_controller.rb
@@ -1,3 +1,27 @@
-class Api::V1::SchoolSanitationController < ApplicationController
-
+module Api
+  module V1
+    class Api::V1::SchoolSanitationController < ApplicationController
+      respond_to :json
+      
+      def index
+        respond_with DistrictGirlSanitationCommunitySchool.all
+      end
+      
+      def show
+        respond_with DistrictGirlSanitationCommunitySchool.find(params[:id])
+      end
+      
+      def create
+        respond_with DistrictGirlSanitationCommunitySchool.create(params[:districtgirlsanitationcommunityschool])
+      end
+      
+      def update
+        respond_with DistrictGirlSanitationCommunitySchool.update(params[:id], params[:districtgirlsanitationcommunityschool])
+      end
+      
+      def destory
+        respond_with DistrictGirlSanitationCommunitySchool.destroy(params[:id])
+      end
+    end
+  end
 end
