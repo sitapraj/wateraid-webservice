dc8899de03488f535e9db48d0c5cc1c649bc8d85
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-06-06 14:07:10 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WateraidWebservice::Application.config.secret_token = '54c76fdb64096316ee197f355cf4da382a56295cb8e9bf73dccfed1639da3b2a4516f177ca8ea46d52c39c7929ca98fa18f9839068cfafd75ac875db8c3133c4'
