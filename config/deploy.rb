e3f0f9c9a14b7defa020e4e878973443b18a7a46
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-07-06 13:43:12 +0545

    Added the server name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "surajt.local"
+set :application, "192.168.1.1"
 set :repository,  "git@192.168.1.1:wateraid-webservice.git"
 
 set :scm, :git
@@ -9,9 +9,9 @@ set :use_sudo, true
 set :deploy_to, "/home/surajt/public_html/#{application}"
 set :deploy_via, :remote_cache
 
-role :web, "surajt.local"                          # Your HTTP server, Apache/etc
-role :app, "surajt.local"                          # This may be the same as your `Web` server
-role :db,  "surajt.local", :primary => true # This is where Rails migrations will run
+role :web, "192.168.1.1"                          # Your HTTP server, Apache/etc
+role :app, "192.168.1.1"                          # This may be the same as your `Web` server
+role :db,  "192.168.1.1", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
