c9b339d039a559e3dba778437c4b207e99bf68d0
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-07-06 13:31:22 +0545

    Added the capistrano for deployment

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'rails', '3.2.6'
 
 gem 'mongoid', '~> 3.0.0.rc'
 
+gem 'therubyracer'
+
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,34 @@
-set :application, "WaterAid-WebServices"
-set :repository,  "set your repository location here"
+set :application, "surajt.local"
+set :repository,  "git@192.168.1.1:wateraid-webservice.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :user, 'surajt'
+set :use_sudo, true
+set :deploy_to, "/home/surajt/public_html/#{application}"
+set :deploy_via, :remote_cache
+
+role :web, "surajt.local"                          # Your HTTP server, Apache/etc
+role :app, "surajt.local"                          # This may be the same as your `Web` server
+role :db,  "surajt.local", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
+after "deploy", "deploy:bundle_gems"
+after "deploy:bundle_gems", "deploy:restart"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ namespace :deploy do
+	 task :bundle_gems do
+   	run "cd #{deploy_to}/current && bundle install vender/gems"
+	 end
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
