98afae6bc4e27a04b472f95108167524ea8cb39b
Suraj Tamang <suraj.tamang@yipl.com.np>
2012-07-09 09:54:50 +0545

    New cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, :git
-set :repository,  "git@devyipl:#{application}.git"
+set :repository,  "git@dev.yipl.com.np:#{application}.git"
 set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
